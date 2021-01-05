d6c09d23d4425f0d876c26d6224f9f0762de66d2
mrbillbbqweb <77015175+mrbillbbqweb@users.noreply.github.com>
2021-01-05 15:26:41 -0500

    Add files via upload

---
+++ assets/plugins/contact/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Mail Setup
+// ==========
+
+// Email address notifications will be sent to
+$to_email = 'talaythaipompanobeach@gmail.com';
+
+// Email subject
+$mail_subject = 'New email via Touche';
+
+// Email content can be modified in sendmail.php
+
+?>
